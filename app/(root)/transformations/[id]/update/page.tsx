6ce24650312408ae36b5184f05cb41e791f5a5b8
priyadarshi7 <priyadarshisatyakam3@gmail.com>
2024-11-01 19:11:10 +0530

    init

---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const updateTransformation = () => {
+  return (
+    <div>updateTransformatio</div>
+  )
+}
+
+export default updateTransformation
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const addTransformation = () => {
+  return (
+    <div>addTransformation</div>
+  )
+}
+
+export default addTransformation
